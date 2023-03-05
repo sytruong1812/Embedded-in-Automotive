fc08f6cd9c34f56ae0b9d5b3af49bd3909fb9588
SY TRUONG <sytruong1812@gmail.com>
2023-03-05 16:42:13 +0700

    Update

--- C/bit.c
+++ C/bit.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int PORTA = 0b00010010;     //12
+int PORTA = 0bxxxxxxxx;    
 
 void digitalWrite(uint8_t pin, uint8_t status)
 {
     if (status == 1)
     {
-        PORTA |= 0b010000000 >> pin;
+        PORTA = PORTA|1 << (7 - pin);
     }
     else if (status == 0)
     {
-        PORTA &= ~(0b10000000 >> pin);      //PORTA = PORTA AND với Đảo bit của(0b10000000 dịch phải bit 3 đơn vị) = 2
+        PORTA &= ~ (1 << (7 - pin));
     }
-    printf("KQ: %d", PORTA);
 }
 
 int main(int argc, char const *argv[])
---
+++ C/malloc.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    uint8_t *data;
+    data = (uint8_t *)malloc(5*sizeof(uint8_t));
+
+    for (int i = 0; i < 5; i++)
+    {
+        data[i] = i*2;
+    }
+    
+    for (int i = 0; i < 5; i++)
+    {
+        printf("i = %d\n", data[i]);
+    }
+
+
+    return 0;
+}
