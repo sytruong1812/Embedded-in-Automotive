1173bd55d5bc5f21c2da499cebd0498bb1b0abd1
SY TRUONG <sytruong1812@gmail.com>
2023-03-09 20:40:45 +0700

    Update

---
+++ C/static_extern/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void DEM(){
+    int x = 0;      //Nếu khai báo vậy hàm được gọi 3 lần xuất ra 0,0,0 => Biến x không được lưu trữ địa chỉ
+    static int x = 0;       //Nếu khai báo vậy hàm được gọi 3 lần xuất ra 1,2,3 => Biến static lưu trữ địa chỉ của biến x,
+                            //và biến static tồn tại hết vòng đời của chương trình
+    printf("Dem: %d\n", x);
+    x++;
+}
+
+extern void Count();    // extern dùng để gọi hàm count() trong file test qua
+
+//extern int MAX();       //Mình không thể extern hàm MAX() bên file test được, vì đã dùng static để định nghĩ hàm đó
+
+int main(int argc, char const *argv[])
+{
+    DEM();
+    DEM();
+    DEM();
+
+    Count();
+
+    //MAX(3,2);
+
+    return 0;
+}
+
+// Build 2 file 1 lúc: 
+// gcc main.c test.c -o main
+// ./main
---
+++ C/static_extern/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+static int c = 5;     //Biến static toàn cục, chỉ có giá trị trong file chứa nó
+                        //Muốn người ta không thể can thiệp, hack vào hàm, thư viện của mình
+                        
+static int MAX(int a, int b){      //Dùng biến static để ngăn người dùng gọi hàm ấy vào
+    int max;                        //Qua file main.c mình không thể extern hàm test() này được
+    if(a>b){
+        max = a;
+    }
+    else{
+        max = b;
+    }
+    return max;
+}
+void Count(){
+    printf("Count: %d\n", c);
+    c++;
+}
