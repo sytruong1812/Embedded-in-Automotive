e1c85127b14907b0cc54c6b2e2d9c4085d50cb31
SY TRUONG <sytruong1812@gmail.com>
2023-03-14 19:09:28 +0700

    Update

--- C/struct & union/main.c
+++ C/struct & union/main.c
@@ -1,14 +1,20 @@
 #include "stdio.h"
 #include "string.h"
 
-char value[5] = {1,2,3,4,5};
+/*strcpy là một hàm trong thư viện string.h trong C/C++. 
+Nó được sử dụng để sao chép nội dung của một chuỗi ký tự đến một chuỗi ký tự khác. 
+Hàm này có hai tham số là một con trỏ đến vùng nhớ của chuỗi ký tự đích và một con trỏ 
+đến vùng nhớ của chuỗi ký tự nguồn. Hàm strcpy sẽ sao chép toàn bộ nội dung của chuỗi 
+ký tự nguồn đến vùng nhớ của chuỗi ký tự đích, bao gồm cả ký tự kết thúc chuỗi '\0'.*/
+
+char value[5] = {1,2,3,6,9};
 
 union data_frame
 {
     struct data
     {
         char id[1];             //1byte
-        int cmd[3];           //3byte
+        char cmd[3];           //3byte
         char check_sum[1];      //1byte
     }sdata;
     char data_store[5];
@@ -18,6 +24,20 @@ union data_frame frame;
 
 int main(int argc, char const *argv[])
 {
-    strcpy(frame.data_store, value);
+    
+    // strcpy(frame.sdata.id, (char *)"1");        // Transmission
+    // strcpy(frame.sdata.cmd, (char *)"236");
+    // strcpy(frame.sdata.check_sum, (char *)"9");
+
+
+    // // strcpy(frame.data_store, value);            // Receive
+    
+    // for (int i = 0; i < 5; i++)                 
+    // {
+    //     printf("%c ", frame.data_store[i]);
+    // }
+    
+    strcpy(frame.sdata.id, (char *)"5");
+    strcpy(frame.data_store, value[4]);
     return 0;
 }
---
+++ C/struct & union/tempCodeRunnerFile.c
@@ -0,0 +1,12 @@
+    // strcpy(frame.sdata.id, (char *)"1");        // Transmission
+    // strcpy(frame.sdata.cmd, (char *)"236");
+    // strcpy(frame.sdata.check_sum, (char *)"9");
+
+
+    // // strcpy(frame.data_store, value);            // Receive
+    
+    // for (int i = 0; i < 5; i++)                 
+    // {
+    //     printf("%c ", frame.data_store[i]);
+    // }
+    
