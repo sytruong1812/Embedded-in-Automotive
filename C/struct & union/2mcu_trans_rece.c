26618489ef1145de820b062a3c17f734bf381991
SY TRUONG <sytruong1812@gmail.com>
2023-03-21 22:54:50 +0700

    Update

--- C/struct & union/2mcu_trans_rece.c
+++ C/struct & union/2mcu_trans_rece.c
@@ -14,7 +14,7 @@ union data_frame
     struct data
     {
         char id[1];             //1byte
-        char cmd[3];           //3byte
+        char cmd[3];            //3byte
         char check_sum[1];      //1byte
     }sdata;
     char data_store[5];
