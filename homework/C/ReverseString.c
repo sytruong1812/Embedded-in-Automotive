0eb635969170bccbc213f4eebd82ab7d3c6bde2d
SY TRUONG <sytruong1812@gmail.com>
2023-03-24 16:24:14 +0700

    Update

---
+++ homework/C/ReverseString.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+char arr[] = {"Make no mistake All your attempts to create a chilling effect a throttling 
+              effect a strangulating effect on open fearless speech relating to public influence will 
+              not stop either Rahul Gandhi or the Congress Party"};
+
+//Đảo chuỗi
+
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
