34de5b547cdfc51196bedc0a251e5676327018fa
SY TRUONG <sytruong1812@gmail.com>
2023-03-25 21:02:07 +0700

    Update ReverseString.c

--- homework/C/ReverseString.c
+++ homework/C/ReverseString.c
@@ -1,17 +1,19 @@
 #include "stdio.h"
+#include "string.h"
 
+#define LENG_ARRAY(arr)  (sizeof(arr)/sizeof(arr[0]))
 
 char InputArray[] = {"Make no mistake. All your attempts to create a chilling effect a throttling effect a strangulating effect on open fearless speech relating to public influence will not stop either Rahul Gandhi or the Congress Party"};
-char OutputArray[sizeof(InputArray)-1];
+char OutputArray[LENG_ARRAY(InputArray)];
 //Đảo ngược chuỗi thành: Party Congress the or Gandhi Rahul.....
 
 
 void ReverseString(const char InArray[], char OutArray[]){
     int count = 0;
     int j = 0;
-    for (int i = sizeof(InputArray); i >= 0; i--)        
+    for (int i = LENG_ARRAY(InputArray); i >= 0; i--)        
     {
-        if(i == sizeof(InputArray) || i == sizeof(InputArray)-1) continue;
+        if(i == LENG_ARRAY(InputArray) || i == LENG_ARRAY(InputArray)-1) continue;
 
         count++;
 
@@ -34,7 +36,7 @@ void ReverseString(const char InArray[], char OutArray[]){
             }
         }
     }
-    for (int i = 0; i < sizeof(OutputArray); i++)
+    for (int i = 0; i < LENG_ARRAY(InputArray); i++)
     {
         printf("%c", OutputArray[i]);
     }  
@@ -42,7 +44,7 @@ void ReverseString(const char InArray[], char OutArray[]){
 
 int main(int argc, char const *argv[])
 {
-    // printf("Leng: %d\n", sizeof(InputArray));
+    // printf("%d\n", sizeof(InputArray));
     ReverseString(InputArray, OutputArray);
     return 0;
 }
