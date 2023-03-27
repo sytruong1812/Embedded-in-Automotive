625b368ffe3cb245ecedbc38c17558fabb49fb03
SY TRUONG <sytruong1812@gmail.com>
2023-03-27 13:19:40 +0700

    Update

--- homework/C/array_handl.c
+++ homework/C/array_handl.c
@@ -2,7 +2,9 @@
 #include "stdint.h"
 #include "string.h"
 
-uint8_t arr[] = {1,2,3,4,6,5,5,3,2,5,5,1,4,1};
+#define LENG_ARRAY(arr) (sizeof(arr)/sizeof(arr[0]))
+
+uint8_t array[] = {0,1,2,3,4,6,5,5,3,2,5,5,1,4,1,0};
 
 //Sắp xếp mảng từ lớn đến bé
 //Liệt kê các phần tử xuất hiện bao nhiêu lần
@@ -11,24 +13,24 @@ void Sort_Array_And_Count_Occurrences(){
     int x;
     int count = 1; 
     printf("Sort array from largest to smallest: ");
-    for (int i = 0; i < strlen(arr); i++){
-        for (int j = i + 1; j < strlen(arr); j++){
-            if(arr[i] < arr[j]){        //Check the previous variable < the following variable
-                x = arr[i];         
-                arr[i] = arr[j];        //Reverse the position of 2 variables
-                arr[j] = x;             
+    for (int i = 0; i < LENG_ARRAY(array); i++){
+        for (int j = i + 1; j < LENG_ARRAY(array); j++){
+            if(array[i] < array[j]){        //Check the previous variable < the following variable
+                x = array[i];         
+                array[i] = array[j];        //Reverse the position of 2 variables
+                array[j] = x;             
             } 
         }
-        printf("%d ", arr[i]);
+        printf("%d ", array[i]);
     }
     printf("\nThe number of occurrences of the variables in the array is:\n");
-    for (int i = 0; i < strlen(arr); i++)
+    for (int i = 0; i < LENG_ARRAY(array); i++)
     {
-        if(arr[i] == arr[i+1]){         //Check the previous variable = the following variable
+        if(array[i] == array[i+1]){         //Check the previous variable = the following variable
             count++;                    //Increase the count variable 
         }
         else{
-            printf("Variable %d appears in the array %d times!\n", arr[i], count); 
+            printf("Variable %d appears in the array %d times!\n", array[i], count); 
             count = 1;                  //Reassign counter = 1, a new count
         } 
     }   
@@ -36,8 +38,9 @@ void Sort_Array_And_Count_Occurrences(){
 
 int main(int argc, char const *argv[])
 {
-    // printf("Size arr: %d\n", sizeof(arr));       //Kích thước của kiểu dữ liệu
-    // printf("Size arr: %d\n", strlen(arr));       //Chiều dài chuỗi
+    // printf("Size array: %d\n", sizeof(array));       //Kích thước của kiểu dữ liệu
+    // printf("Size array: %d\n", strlen(array));       //Chiều dài chuỗi
+    // printf("Size array: %d\n", LENG_ARRAY(array));
     Sort_Array_And_Count_Occurrences();
 
     return 0;
